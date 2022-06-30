7284d2a049ca166db20b3d2768f8c5db1ff3c78d
jiaruifang <fangjiarui123@gmail.com>
2022-06-30 18:07:22 +0800

    polish

--- colossalai_zero.py
+++ colossalai_zero.py
@@ -2,5 +2,6 @@ from colossalai.zero.shard_utils import TensorShardStrategy
 
 zero = dict(model_config=dict(shard_strategy=TensorShardStrategy(),
                               tensor_placement_policy="auto",
-                              reuse_fp16_shard=True),
-            optimizer_config=dict(gpu_margin_mem_ratio=0.2))
+                              reuse_fp16_shard=True,
+                              warmup_non_model_data_ratio=0.9),
+            optimizer_config=dict(gpu_margin_mem_ratio=0.8))
