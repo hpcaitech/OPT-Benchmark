6f8034f1861e5714ef2cb95b73380ba1f6d77f98
Frank Lee <somerlee.9@gmail.com>
2022-10-06 10:10:20 +0800

    added mode (#12)
    * added mode
    
    * polish code

--- run_opt_clm_colossalai_new.sh
+++ run_opt_clm_colossalai_new.sh
@@ -2,6 +2,7 @@ export BS=${BS:-32}
 export MEMCAP=${MEMCAP:-0}
 export MODEL=${MODEL:-"13b"}
 export GPUNUM=${GPUNUM:-8}
+export TENSOR_PLACEMENT_POLICY=${TENSOR_PLACEMENT_POLICY:-"auto"}
 
 colossalai run --nproc_per_node ${GPUNUM} \
     --master_port 29600 \
@@ -11,4 +12,5 @@ colossalai run --nproc_per_node ${GPUNUM} \
     --model_name_or_path facebook/opt-${MODEL} \
     --output_dir /tmp/test-clm \
     --mem_cap ${MEMCAP} \
+    --policy ${TENSOR_PLACEMENT_POLICY} \
     --per_device_train_batch_size ${BS} 2>&1 | tee ./logs/colo_${MODEL}_bs_${BS}_cap_${MEMCAP}_gpu_${GPUNUM}.log
